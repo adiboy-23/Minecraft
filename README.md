93a2e61ca49268f0b6bb64c93cbd242dd1070c86
adiboy-23 <kadiansahab123gmail.com>
2025-04-16 17:46:24 +0530

    add terminal nonblock input

---
+++ README.md
@@ -0,0 +1,3 @@
+## MINECRAFT IN TERMINAL
+
+gcc -o minecraft minecraft.c && ./minecraft
--- minecraft.c
+++ minecraft.c
@@ -1,4 +1,30 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <termios.h>
+#include <stdlib.h>
+#include <math.h>
+
+static struct termios old_termios, new_termios;
+
+void init_terminal() {
+    tcgetattr(STDIN_FILENO, &old_termios);
+    new_termios = old_termios;
+    new_termios.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_termios);
+    fcntl(STDIN_FILENO, F_SETFL, fcntl(STDIN_FILENO, F_GETFL, 0) | O_NONBLOCK);
+    fflush(stdout);
+}
+
+void restore_terminal() {
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_termios);
+    printf("terminal restored");
+}
+
 
 int main() { 
+    init_terminal();
+    restore_terminal();
     return 0; 
 }
